5c0bf06540c8efdb7b4c14207a3e9a91d9099dfe
dvejmz <dvejmz@gmail.com>
2019-02-05 12:43:08 +0000

    Set up express and add basic /api route

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,22 @@
+const express = require('express');
+
 module.exports = () => {
+    const DEFAULT_PORT = 8080;
+    const app = express();
+    init();
+
     async function run() {
+        init();
+        app.listen(DEFAULT_PORT, () => {
+            console.log(`Server listening on http://localhost:${DEFAULT_PORT} ...`);
+        });
+    }
+
+    function init() {
+        app.use(express.static('public'));
+        app.get('/api', (req, res) => {
+            return res.json({ response: 'ok' });
+        })
     }
 
     return {
